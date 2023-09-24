3f9c8b43320e5d00cf8da2057d837c9f19785030
lh <1072344372@qq.com>
2023-09-24 12:51:54 +0800

    1.2 springmvc配置完成并创建TestController完成测试

---
+++ src/main/java/asia/lhweb/furn/controller/TestController.java
@@ -0,0 +1,18 @@
+package asia.lhweb.furn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author :罗汉
+ * @date : 2023/9/24
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/hi")
+    public String hi(){
+        System.out.println("hi");
+        return "hi";
+    }
+}
